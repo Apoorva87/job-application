bf38161b022b66f92eb2dcdd26ee74607dd84898
qatester <qatester@jadepuget.local>
2024-04-29 13:04:55 -0700

    UserInfo for applications

---
+++ UserInfo.py
@@ -0,0 +1,106 @@
+#!bin/python3
+import os
+
+email='yatin.karnik@gmail.com'
+password='JobHunt2024*'
+social_last_four='2117'
+
+name='Yatin Karnik'
+first='Yatin'
+last='Karnik'
+
+today='04/29/2024'
+
+resume={
+        'pm' : os.path.abspath('/Users/qatester/Downloads/Yatin.docx'),
+        }
+
+job_1_str="""- Developed and launched a user-centric mobile application using advanced AI algorithms to enable efficient loan comparisons across digital platforms, significantly enhancing user experience and decision-making efficiency. (https://confer.today)
+    - Engineered a cutting-edge digital mortgage platform that expanded borrower opportunities to reduce closing costs by 88%, showcasing deep expertise in e-commerce solutions.
+    - Guided a cross-platform app launch within 90 days through meticulous agile project management and utilization of AWS, Google Cloud, enhancing development efficiency and product quality by 30%.
+    - Spearheaded cross-functional teams under agile frameworks to enhance market responsiveness and tech adaptability, achieving a 25% faster time-to-market and increasing product adoption by 15% through effective leadership.
+    - Led the strategic pivot of Confer Inc. to a B2B model, overseeing the launch of innovative financial software solutions for banks, which propelled a 25% increase in revenue and solidified market presence."""
+
+job_2_str="""- Revolutionized Home Lending Fee Strategy, boosting regulatory compliance and operational efficiency which slashed error rates dramatically from 7% to less than 1%, by implementing agile methodologies and cross-functional team management.
+- Spearheaded the agile-driven launch of the 'Fee Service' engine, integrating over 1200 settlement agents which enhanced transaction efficiency and cut closing costs by 15%, showcasing strong project management and digital technology application.
+ - Directed over 25 global remediation projects in collaboration with marketing, finance, legal, and operations teams, enhancing compliance with CFPB/RESPA/TILA standards under stringent agile practices. This cross-functional effort demonstrated adeptness in large-scale project management and regulatory adherence, ensuring cohesive strategy and execution across departments.
+ - Implemented robust project management frameworks that improved product innovation cycles by 30% and operational agility by 25%, facilitating effective cross-functional collaborations and aligning with corporate strategic goals.
+ - Developed and deployed innovative digital mortgage platforms that enhanced e-commerce transaction capabilities, boosting user engagement by 46% and financial throughput by 35%.
+ - Led the agile transformation in operational practices, enhancing the speed and efficiency of regulatory project deliveries by 22%, and fostering an environment of continuous improvement and innovation. This strategic shift significantly streamlined compliance with federal regulations, contributing to a 18% reduction in compliance issues.
+ - Boosted VOI, VOE, and VOD process accuracy and efficiency across international teams, decreasing turnaround by 1 day and led Vendor Support Teams across five locations to a 15% gain in Retail Fulfillment productivity with strategic tech initiatives.
+ - Championed cross-functional initiatives, harmonizing tech developments with business and regulatory demands, which streamlined compliance, reduced audit findings by 20%, and achieved annual cost savings of $750k.
+"""
+
+job_3_str=""" - Pioneered the SDLC for a groundbreaking product recommendation tool, markedly boosting digital customer engagement and securing the prestigious J.D. Power Associates Award for three consecutive years.
+ - Championed the launch of Wells Fargo's first mobile app for home mortgage services and an award-winning product recommendation tool, developed in collaboration with UX/UI teams to set a digital standard across iOS, Android, and Blackberry platforms. This initiative, enhancing user engagement and support, was adopted by 16,000 mortgage loan originators and significantly improved our digital customer service capabilities.
+ - Spearheaded the 'Fee Engine' design, a business rules engine using automation to revamp fee calculations, enhancing accuracy and eliminating over-tolerance losses by $2M annually, driving operational improvements.
+ - Implemented process automation strategies that yielded $12M in annual cost savings, streamlining technology solutions in pricing and fee estimation.
+ - Directed the design of the HARP/HAMP triage tool, partnering with UX/UI teams to enhance user interface and customer experience, which reviewed 3,000 applications in its first week and improved mortgage affordability and diversity. This project showcased our commitment to using digital solutions to enhance customer service."""
+
+job_4_str="""- Developed a policy lookup tool for 12,000 mortgage loan originators, cutting lookup times and elevating match accuracy by 40%.
+- Created a mortgage recommendation tool, raising mortgage acceptance by 20% and increasing customer satisfaction by offering borrowers better financial terms.
+- Designed and deployed a mortgage reconfiguration algorithm, augmenting approval rates by 25% for previously at-risk mortgages by aligning with alternative investor guidelines.
+"""
+
+jobs=[
+    {
+    'title' : 'Business Operations & Strategy Consultant',
+    'company' : 'Confer Inc.',
+    'location' : 'Dallas, TX',
+    'current' : True,
+    'from' : '11/2021',
+    'end' : '01/2024',
+    'description' : job_1_str,
+    'other' : ''
+    },
+    {
+    'title' : 'Senior Vice President, Head of National Operations & Head of Fee Strategy & Governance',
+    'company' : 'Wells Fargo',
+    'location' : 'Dallas, TX',
+    'current' : False,
+    'from' : '04/2014',
+    'end' : '11/2021',
+    'description' : job_2_str,
+    'other' : ''
+    },
+    {
+    'title' : 'Vice President, Sales and Services System Office',
+    'company' : 'Wells Fargo',
+    'location' : 'Des Moines, IA',
+    'current' : False,
+    'from' : '05/2007',
+    'end' : '04/2014',
+    'description' : job_3_str,
+    'other' : ''
+    },
+    {
+    'title' : 'Sr. Business Analyst',
+    'company' : 'Wells Fargo',
+    'location' : 'Des Moines, IA',
+    'current' : False,
+    'from' : '12/2004',
+    'end' : '05/2007',
+    'description' : job_4_str,
+    'other' : ''
+    },
+    ]
+
+education=[
+        {
+        'school' : 'University of Houston-Clear Lake',   
+        'degree' : 'M.S.',   
+        'field' : 'Computer and Information Science',
+        'gpa' : '3.5/4',
+        'end' : '2004',
+        'from' : '2002',
+        },
+        {
+        'school' : 'Nagpur University',   
+        'degree' : 'B.S.',   
+        'field' : 'Computer and Information Science',
+        'gpa' : '3.5/4',
+        'end' : '2001',
+        'from' : '1997',
+        },
+        ]
+
